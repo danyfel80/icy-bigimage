99c722d9c316a2b1ebc033a326c7faf30137055f
Daniel Felipe Gonzalez Obando <danyfel80@gmail.com>
2016-07-05 10:06:33 +0200

    Saver implemented and tested with plugin

---
+++ BigImageTools/src/plugins/danyfel80/bigimage/io/SaveBigImage.java
@@ -0,0 +1,142 @@
+package plugins.danyfel80.bigimage.io;
+
+import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.io.File;
+import java.io.IOException;
+
+import algorithms.danyfel80.bigimage.io.BigImageWriter;
+import icy.image.IcyBufferedImage;
+import icy.image.IcyBufferedImageUtil;
+import loci.common.services.ServiceException;
+import loci.formats.FormatException;
+import plugins.adufour.ezplug.EzPlug;
+import plugins.adufour.ezplug.EzStoppable;
+import plugins.adufour.ezplug.EzVarFile;
+import plugins.adufour.ezplug.EzVarInteger;
+import plugins.adufour.ezplug.EzVarSequence;
+
+/**
+ * This class allows to save a big image using tiles.
+ * @author Daniel Felipe Gonzalez Obando
+ */
+public class SaveBigImage extends EzPlug implements EzStoppable {
+	
+	EzVarSequence inSeq = new EzVarSequence("Sequence");
+	EzVarFile inFile = new EzVarFile("File", "");
+	EzVarInteger inTileSize = new EzVarInteger("Tile size");
+
+	/* (non-Javadoc)
+	 * @see plugins.adufour.ezplug.EzPlug#initialize()
+	 */
+	@Override
+	protected void initialize() {
+		addEzComponent(inSeq);
+		addEzComponent(inFile);
+		addEzComponent(inTileSize);
+	}
+	
+	/* (non-Javadoc)
+	 * @see plugins.adufour.ezplug.EzPlug#execute()
+	 */
+	@Override
+	protected void execute() {
+		IcyBufferedImage ibi = inSeq.getValue().getFirstImage();
+		
+		File file = inFile.getValue();
+		int tileSize = inTileSize.getValue();
+		int tileSizeX = tileSize;
+		int tileSizeY = tileSize;
+
+		this.getUI().setProgressBarMessage("Saving");
+
+		int x, y;
+		int w, h;
+		int sizeX, sizeY;
+		int n, m;
+		int diffWidth, diffHeight;
+
+		sizeX = ibi.getWidth();
+		sizeY = ibi.getHeight();
+		if (tileSizeX <= 0)
+			tileSizeX = sizeX;
+		if (tileSizeY <= 0)
+			tileSizeY = sizeY;
+		n = sizeX / tileSizeX;
+		m = sizeY / tileSizeY;
+		if (n == 0) {
+			tileSizeX = sizeX;
+			n = 1;
+		}
+		if (m == 0) {
+			tileSizeY = sizeY;
+			m = 1;
+		}
+		diffWidth = sizeX - n * tileSizeX;
+		diffHeight = sizeY - m * tileSizeY;
+		if (diffWidth > 0)
+			n++;
+		if (diffHeight > 0)
+			m++;
+
+		BigImageWriter saver = null;
+		try {
+			saver = new BigImageWriter(file, new Dimension(ibi.getWidth(), ibi.getHeight()), ibi.getSizeC(),
+					ibi.getDataType_(), new Dimension(tileSizeX, tileSizeY));
+		} catch (ServiceException | FormatException | IOException e1) {
+			e1.printStackTrace();
+			return;
+		}
+		
+		try {
+			for (int i = 0; i < m; i++) {
+				if (diffHeight > 0 && i == (m - 1)) {
+					y = sizeY - diffHeight;
+					h = diffHeight;
+				} else {
+					y = tileSizeY * i;
+					h = tileSizeY;
+				}
+				for (int j = 0; j < n; j++) {
+					if (diffWidth > 0 && j == (n - 1)) {
+						x = sizeX - diffWidth;
+						w = diffWidth;
+					} else {
+						x = tileSizeX * j;
+						w = tileSizeX;
+					}
+					
+					Rectangle rect = new Rectangle(x, y, w, h);
+					Point point = new Point(x, y);
+//					System.out.println("Saving tile " + rect + " at " + point);
+					saver.saveTile(IcyBufferedImageUtil.getSubImage(ibi, rect), point);
+
+				}
+			}
+
+		} catch (FormatException | IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				saver.closeWriter();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		this.getUI().setProgressBarMessage("Done");
+	}
+
+	/* (non-Javadoc)
+	 * @see plugins.adufour.ezplug.EzPlug#clean()
+	 */
+	@Override
+	public void clean() {
+		// TODO Auto-generated method stub
+
+	}
+
+	
+
+}
