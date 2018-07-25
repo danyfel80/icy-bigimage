ebaaa65b0058c3d90965dbcb871500bf366e05d0
Daniel Felipe Gonzalez Obando <danyfel80@gmail.com>
2018-07-25 14:52:24 +0200

    Added BigImageTools library class

---
+++ BigImageTools/src/main/java/plugins/danyfel80/bigimage/io/BigImageTools.java
@@ -0,0 +1,8 @@
+package plugins.danyfel80.bigimage.io;
+
+import icy.plugin.abstract_.Plugin;
+import icy.plugin.interface_.PluginLibrary;
+
+public class BigImageTools extends Plugin implements PluginLibrary {
+
+}
