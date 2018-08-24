ed309ff1d254942e4c8f4e0472edf7044d3e9385
Daniel Felipe Gonzalez Obando <danyfel80@gmail.com>
2018-08-24 10:48:59 +0200

    large sequence tile provider protected

--- BigImageTools/src/main/java/algorithms/danyfel80/io/sequence/tileprovider/LargeSequenceTileProvider.java
+++ BigImageTools/src/main/java/algorithms/danyfel80/io/sequence/tileprovider/LargeSequenceTileProvider.java
@@ -23,10 +23,26 @@ public class LargeSequenceTileProvider implements ITileProvider {
 	private Dimension currentTileSize;
 
 	public LargeSequenceTileProvider() {
-		tileSize = new Dimension();
+		setTileSize(new Dimension());
 	}
 
-	private LociImporterPlugin getImporter() {
+	public Dimension getTileSize() {
+		return tileSize;
+	}
+
+	public void setTileSize(Dimension tileSize) {
+		this.tileSize.setSize(tileSize);
+	}
+
+	protected boolean isProviderPrepared() {
+		return providerPrepared;
+	}
+
+	protected void setProviderPrepared(boolean prepared) {
+		providerPrepared = prepared;
+	}
+
+	protected LociImporterPlugin getImporter() {
 		return importer;
 	}
 
@@ -37,26 +53,34 @@ public class LargeSequenceTileProvider implements ITileProvider {
 	@Override
 	public IcyBufferedImage getTile(Point tileIndex) throws IOException {
 		prepareProvider();
-		currentTileIndex = tileIndex;
-		getTileRectangle();
+		setCurrentTileIndex(tileIndex);
+		computeTileRectangle();
 		IcyBufferedImage tileImage;
 		try {
-			tileImage = importer.getImage(0, 0, currentTileRectangle, 0, 0);
+			tileImage = getImporter().getImage(0, 0, getCurrentTileRectangle(), 0, 0);
 		} catch (UnsupportedFormatException e) {
 			throw new IOException(e);
 		}
 		return tileImage;
 	}
 
-	private void prepareProvider() throws IOException {
+	protected Point getCurrentTileIndex() {
+		return currentTileIndex;
+	}
+
+	protected void setCurrentTileIndex(Point tileIndex) {
+		currentTileIndex = tileIndex;
+	}
+
+	protected void prepareProvider() throws IOException {
 		if (!isProviderPrepared()) {
 			if (getImporter() == null)
 				throw new IOException("No importer specified");
 
-			if (tileSize.width * tileSize.height == 0) {
+			if (getTileSize() == null || getTileSize().width * getTileSize().height == 0) {
 				try {
-					setTileSize(new Dimension(importer.getTileWidth(0), importer.getTileHeight(0)));
-					metatadata = importer.getOMEXMLMetaData();
+					setTileSize(new Dimension(getImporter().getTileWidth(0), getImporter().getTileHeight(0)));
+					setMetatadata(getImporter().getOMEXMLMetaData());
 				} catch (UnsupportedFormatException e) {
 					throw new IOException(e);
 				}
@@ -66,45 +90,65 @@ public class LargeSequenceTileProvider implements ITileProvider {
 		}
 	}
 
-	private void getTileRectangle() {
-		getCurrentTilePosition();
-		getCurrentTileSize();
-		currentTileRectangle = new Rectangle(currentTilePosition.x, currentTilePosition.y, currentTileSize.width,
-				currentTileSize.height);
+	protected OMEXMLMetadata getMetatadata() {
+		return metatadata;
 	}
 
-	private void getCurrentTilePosition() {
-		if (currentTilePosition == null)
-			currentTilePosition = new Point();
-
-		currentTilePosition.setLocation(currentTileIndex.x * tileSize.width, currentTileIndex.y * tileSize.height);
+	protected void setMetatadata(OMEXMLMetadata metatadata) {
+		this.metatadata = metatadata;
 	}
 
-	private void getCurrentTileSize() {
-		if (currentTileSize == null)
-			currentTileSize = new Dimension();
+	protected void computeTileRectangle() {
+		computeCurrentTilePosition();
+		computeCurrentTileSize();
+		setCurrentTileRectangle(new Rectangle(getCurrentTilePosition().x, getCurrentTilePosition().y,
+				getCurrentTileSize().width, getCurrentTileSize().height));
+	}
 
-		currentTileSize.width = tileSize.width;
-		currentTileSize.height = tileSize.height;
+	protected Rectangle getCurrentTileRectangle() {
+		return currentTileRectangle;
+	}
 
-		if (metatadata.getPixelsSizeX(0).getValue() <= currentTilePosition.x + tileSize.width) {
-			currentTileSize.width = metatadata.getPixelsSizeX(0).getValue() - currentTilePosition.x;
+	protected void setCurrentTileRectangle(Rectangle currentTileRectangle) {
+		this.currentTileRectangle = currentTileRectangle;
+	}
+
+	protected void computeCurrentTilePosition() {
+		if (getCurrentTilePosition() == null)
+			setCurrentTilePosition(new Point());
+
+		getCurrentTilePosition().setLocation(getCurrentTileIndex().x * getTileSize().width,
+				getCurrentTileIndex().y * getTileSize().height);
+	}
+
+	protected Point getCurrentTilePosition() {
+		return currentTilePosition;
+	}
+
+	protected void setCurrentTilePosition(Point currentTilePosition) {
+		this.currentTilePosition = currentTilePosition;
+	}
+
+	protected void computeCurrentTileSize() {
+		if (getCurrentTileSize() == null)
+			setCurrentTileSize(new Dimension());
+
+		getCurrentTileSize().setSize(getTileSize().width, getTileSize().height);
+
+		if (getMetatadata().getPixelsSizeX(0).getValue() <= getCurrentTilePosition().x + getTileSize().width) {
+			getCurrentTileSize().width = getMetatadata().getPixelsSizeX(0).getValue() - getCurrentTilePosition().x;
 		}
-		if (metatadata.getPixelsSizeY(0).getValue() <= currentTilePosition.y + tileSize.height) {
-			currentTileSize.height = metatadata.getPixelsSizeY(0).getValue() - currentTilePosition.y;
+		if (getMetatadata().getPixelsSizeY(0).getValue() <= getCurrentTilePosition().y + getTileSize().height) {
+			getCurrentTileSize().height = getMetatadata().getPixelsSizeY(0).getValue() - getCurrentTilePosition().y;
 		}
 	}
 
-	private boolean isProviderPrepared() {
-		return providerPrepared;
+	protected Dimension getCurrentTileSize() {
+		return currentTileSize;
 	}
 
-	private void setProviderPrepared(boolean prepared) {
-		providerPrepared = prepared;
-	}
-
-	public void setTileSize(Dimension tileSize) {
-		this.tileSize.setSize(tileSize);
+	protected void setCurrentTileSize(Dimension currentTileSize) {
+		this.currentTileSize = currentTileSize;
 	}
 
 }
